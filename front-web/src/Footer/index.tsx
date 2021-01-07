a4a30cffbdcad6b4de2b157a227c22b4611490e2
Fernando-Gouvea <fernando.gouvea.lfg@gmail.com>
2021-01-07 18:53:01 -0300

    Add footer

---
+++ front-web/src/Footer/index.tsx
@@ -0,0 +1,32 @@
+import './styles.css';
+import  {ReactComponent as Logo} from './Logo.svg';
+import  {ReactComponent as MainImage} from './main.svg';
+import  {ReactComponent as YouTubeIcon} from './Youtube.svg';
+import  {ReactComponent as InstagramIcon} from './Instagram.svg';
+import  {ReactComponent as LinkedinIcon} from './Linkedin.svg';
+
+function Footer(){
+    return(
+        <footer className="main-footer">
+       
+            App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
+            <div className="footer-icons">
+                <a href="https://www.youtube.com/c/DevSuperior" target="_new">
+                    <YouTubeIcon />
+                </a>
+                <a href="https://www.linkedin.com/school/devsuperior/" target="_new">
+                    <LinkedinIcon />
+                </a>
+                <a href="https://www.instagram.com/devsuperior.ig/">
+                    <InstagramIcon />
+                </a>
+
+
+            </div>
+
+        </footer>
+    
+    )
+}
+
+export default Footer;
